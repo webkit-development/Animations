77d518d271334bb03de7a9f1bfdd05f25cafb119
Kevin Stradtman <webkit.development.systems@gmail.com>
2022-03-23 15:52:06 -0400

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Kevin Stradtman on 3/23/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
