9961573a6830b48cc41bf837821285f475d965cf
Kevin Stradtman <webkit.development.systems@gmail.com>
2022-03-23 19:52:30 -0400

    Added some more animations

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -10,24 +10,59 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var animationAmount = 1.0
+    @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
     var body: some View {
-        Button("Tap Me") {
-            animationAmount += 1
+        ScrollView {
+            Spacer()
+            Stepper("Scale Amount", value: $animationAmount.animation(
+                .easeInOut(duration: 1)
+                    .repeatCount(3, autoreverses: true)
+            ), in: 1...10)
+            Spacer()
+            Button("Tap Me") {
+                animationAmount += 1
+            }
+            .padding(50)
+            .background(.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .overlay(
+                Circle()
+                    .stroke(.red)
+                    .scaleEffect(animationAmount)
+                    .opacity(2 - animationAmount)
+            )
+            .scaleEffect(animationAmount)
+            .animation(.easeInOut(duration: 1)
+                        .repeatForever(autoreverses: false),
+                       value: animationAmount)
+            Spacer()
+            
+            Button("Tap Me") {
+                enabled.toggle()
+            }
+            .frame(width: 200, height: 200)
+            .background(enabled ? .blue : .red)
+            .foregroundColor(.white)
+            .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
+            .animation(.default, value: enabled)
+            Spacer()
+            Section {
+                LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .topLeading, endPoint: .bottomLeading)
+                    .frame(width: 300, height: 200)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .offset(dragAmount)
+                    .gesture(
+                        DragGesture()
+                            .onChanged { dragAmount = $0.translation }
+                            .onEnded { _ in dragAmount = .zero }
+                        
+                    )
+                    .animation(.spring(), value: dragAmount)
+            }
+            Spacer()
         }
-        .padding(50)
-        .background(.red)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-        .overlay(
-            Circle()
-                .stroke(.red)
-                .scaleEffect(animationAmount)
-                .opacity(2 - animationAmount)
-        )
-        .scaleEffect(animationAmount)
-        .animation(.easeInOut(duration: 1)
-                    .repeatForever(autoreverses: false),
-                    value: animationAmount)
     }
 }
 
