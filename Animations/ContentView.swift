240465d0063d4b671eae4f67baedb93d1961460a
Kevin Stradtman <webkit.development.systems@gmail.com>
2022-03-23 17:43:22 -0400

    Added some to contentview

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -4,13 +4,30 @@
 //
 //  Created by Kevin Stradtman on 3/23/22.
 //
-
+//.blur(radius: (animationAmount - 1) * 3)
+//.animation(.interpolatingSpring(stiffness: 50, damping: 1), value: animationAmount)
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount = 1.0
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("Tap Me") {
+            animationAmount += 1
+        }
+        .padding(50)
+        .background(.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .overlay(
+            Circle()
+                .stroke(.red)
+                .scaleEffect(animationAmount)
+                .opacity(2 - animationAmount)
+        )
+        .scaleEffect(animationAmount)
+        .animation(.easeInOut(duration: 1)
+                    .repeatForever(autoreverses: false),
+                    value: animationAmount)
     }
 }
 
